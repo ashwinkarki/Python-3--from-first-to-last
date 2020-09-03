476966079d4a728291fcc75a6c46f9a726d75a1c
ashwin.karki <ashwin.karki123@yahoo.com>
2020-09-03 12:48:37 +0545

    done upto classes

---
+++ Chapter-14/student.py
@@ -0,0 +1,20 @@
+def my_function():
+    print("Hello World")
+
+# Defining our variable
+name = "Nicholas"
+
+def my_function2():
+    print("Hello World 2")
+
+# Defining a class
+class Student:
+    def __init__(self, name, course):
+        self.course = course
+        self.name = name
+
+    def get_student_details(self):
+        print("Your name is " + self.name + ".")
+        print("You are studying " + self.course)
+        
+
